a6d276d51a27fe3b12e71300368d4108d45f7cb9
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-07-01 09:26:12 -0300

    Room Navigation by Id

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,14 @@ import { useHistory } from "react-router-dom";
 
 import "../styles/auth.scss";
 import { useAuth } from "../hooks/useAuth";
+import { FormEvent } from "react";
+import { useState } from "react";
+import { database } from "../services/firebase";
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [ roomCode, setRoomCode ] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
@@ -17,6 +21,21 @@ export function Home() {
     }
     
     history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+    if (!roomRef.exists()) {
+      alert('Room does not exist!');
+      return;
+    }
+    
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -31,17 +50,26 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="logo" />
+
           <button onClick={handleCreateRoom} className="createRoom">
             <img src={googleIconImg} alt="google-icon" />
             Crie sua sala com o Google
           </button>
+
           <div className="separator">ou entre em uma sala</div>
-          <form action="">
-            <input type="text" placeholder="Digite o código da sala" />
+
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">
               Entrar na sala
             </Button>
           </form>
+
         </div>
       </main>
     </div>
